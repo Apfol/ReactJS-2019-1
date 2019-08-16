206e34e609b72552afea6bdf359f71e5885a1a65
jordanlosa97 <jordanlosa@unisabana.edu.co>
2019-08-16 12:10:02 -0500

    primera parte

--- proyecto_bienestar/src/App.js
+++ proyecto_bienestar/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MissingObject from './Resources/Components/MissingObject';
 
 function App() {
+  const object = [
+    {name: "Calculadora", 
+    lugar: "Biblioteca",
+    encontrado_por: "Luis"}
+  ] 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <MissingObject object = {object}></MissingObject>
     </div>
   );
 }
