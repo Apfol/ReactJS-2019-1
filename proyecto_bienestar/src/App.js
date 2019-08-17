29c1ab60bcf0b7bee1f476c799b56438f8d07d13
jordanlosa97 <jordanlosa@unisabana.edu.co>
2019-08-17 14:03:29 -0500

    arreglando errores

--- proyecto_bienestar/src/App.js
+++ proyecto_bienestar/src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import './App.css';
-import MissingObject from './Resources/Components/MissingObject';
+
 import MissingObjectList from './Resources/Components/MissingObjectList';
 
-function App() {
-  const object = [
-    {name: "Calculadora", 
-    location: "Biblioteca",
-    by: "Luis"}
-  ] 
+function App() {  
   return (
     <div className="App">
       <MissingObjectList/>
