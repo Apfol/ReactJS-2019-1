c9cff896b741f09aba8c3db567daaabf1d5722b1
danielNempeque <daniel.nempeque@gmail.com>
2019-08-18 16:28:31 -0500

    Project ini

---
+++ gamer-in/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "./Home.css"
+class Home extends Component {
+	render() {
+		return (
+			<div className="Background-1-hex">
+				<p>HOME COMPONENT</p>
+			</div>
+		);
+	}
+}
+
+export default Home;
