958c3af45ae8676135b8a98eca240e15240c447a
Pachomen <julianalbertop7@gmail.com>
2019-08-20 21:19:17 -0500

    Trabajo de CV y Puzzle

---
+++ sliding_puzzle/Puzzles/main.js
@@ -0,0 +1,53 @@
+//En base al desarollo de la pagina https://www.101computing.net/sliding-puzzle/
+
+  
+  function shuffle() {
+
+  for (var row=1;row<=3;row++) { 
+     for (var column=1;column<=3;column++) { 
+    
+      var row2=Math.floor(Math.random()*3 + 1);
+      var column2=Math.floor(Math.random()*3 + 1); 
+       
+      cambioImagen("cell"+row+column,"cell"+row2+column2);
+    } 
+  } 
+  }
+  
+  function clickVacio(row,column) {
+    var cell = document.getElementById("cell"+row+column);
+    var tile = cell.className;
+    if (tile!="cuadro9") { 
+         if (column<3) {
+           if ( document.getElementById("cell"+row+(column+1)).className=="cuadro9") {
+             cambioImagen("cell"+row+column,"cell"+row+(column+1));
+             return;
+           }
+         }
+         if (column>1) {
+           if ( document.getElementById("cell"+row+(column-1)).className=="cuadro9") {
+             cambioImagen("cell"+row+column,"cell"+row+(column-1));
+             return;
+           }
+         }
+         if (row>1) {
+           if ( document.getElementById("cell"+(row-1)+column).className=="cuadro9") {
+             cambioImagen("cell"+row+column,"cell"+(row-1)+column);
+             return;
+           }
+         }
+         if (row<3) {
+           if ( document.getElementById("cell"+(row+1)+column).className=="cuadro9") {
+             cambioImagen("cell"+row+column,"cell"+(row+1)+column);
+             return;
+           }
+         } 
+    }
+    
+  }
+
+  function cambioImagen(cell1,cell2) {
+    var temp = document.getElementById(cell1).className;
+    document.getElementById(cell1).className = document.getElementById(cell2).className;
+    document.getElementById(cell2).className = temp;
+  }
---
+++ web_curricilum_vitae/CV/nvdsjfndj.js
@@ -0,0 +1,15 @@
+var a = 0;
+var b = 0;
+var c = 0;
+
+function fib(c) {
+    while(n = true){
+       a = b * b ;
+       b++;
+       if(a <= c){
+           n = false;
+           return b[0];
+       } 
+    }
+}
+
