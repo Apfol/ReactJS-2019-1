bce3e20b61f61704403ffdd595f5e1d3debc5ae0
THEY4N <36862802+THEY4N@users.noreply.github.com>
2019-09-09 14:56:37 -0500

    To_do_list_app

---
+++ to_do_list/src/Components/TodoList/TodoItems.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import itemz from './TodoItems.css';
+
+class TodoItems extends Component {
+  createTasks = item => {
+    return (
+      <li key={item.key} className={itemz.listo} onClick={() => this.props.deleteItem(item.key)}>
+        <div className={itemz.iconContainer}><div className={itemz.icon}></div></div><div className={itemz.item}>{item.text}</div> 
+      </li>
+    )
+  }
+  render() {
+    const todoEntries = this.props.entries
+    const listItems = todoEntries.map(this.createTasks)
+
+    return <ul className={itemz.theList}>{listItems}</ul>
+  }
+}
+
+export default TodoItems;
