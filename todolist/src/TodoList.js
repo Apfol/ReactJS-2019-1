588ed07a39e244e559c28e933e072b4297cb97ae
franciscoobohu <franciscobohu@unisabana.edu.co>
2019-09-10 03:11:29 -0500

    taller 3

---
+++ todolist/src/TodoList.js
@@ -0,0 +1,60 @@
+import React, { Component} from "react";
+import TodoItems from "./TodoItems.js";
+import "./TodoList.css";
+
+class TodoList extends Component{
+	constructor(props){
+		super(props);
+		this.state={
+			items:[]
+		};
+		this.addItem= this.addItem.bind(this);
+		this.deletItem= this.deletItem.bind(this);
+	}
+	addItem(e){
+		if (this._inputElement.value !==""){
+			var newItem = {
+				text: this._inputElement.value,
+				ley: Date.now()
+			};
+			this.setState((prevState)=>{
+				return{
+					items: prevState.items.concat(newItem)
+				};
+			});
+		}
+		this._inputElement.value ="";
+		console.log(this.state.items);
+		e.preventDefault();
+	}
+
+	deletItem(key){
+		var firteredItems=this.state.items.filter(function(item){
+			return(item.key !== key)
+		});
+		this.setState({
+			items: firteredItems
+		});
+	}
+
+
+	render(){
+		return(
+			<div className="todoListMain">
+			<div className="header">
+			<form onSubmit={this.addItem}>
+				<input ref={(a)=>this._inputElement= a}
+					placeholder="enter task">
+				</input>
+				<button type="submit">add</button>
+			</form>
+			</div>
+			<TodoItems entries={this.state.items}
+					   delete={this.deletItem}
+					   />
+			</div>
+		);
+
+	}
+}
+export default TodoList;
