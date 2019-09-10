8a6c81a0302ecb7cf9bb7e8d9d9d79c8b48cd952
Apfol <andresram@unisabana.edu.co>
2019-09-10 17:11:02 -0500

    Modificado Notices

--- proyecto-final/src/body/Notices.js
+++ proyecto-final/src/body/Notices.js
@@ -5,11 +5,11 @@ class Notices extends React.Component {
     render = () => {
         return (
             <li class="nav-item noticia">
-                <div className = "row notices">
-                    <div className = "col-md-6 cols">
-                        <Image className="noticeImg" src={this.props.notice} fluid/>
+                <div className="row notices">
+                    <div className="col-md-6 cols">
+                        <Image className="noticeImg" src={this.props.notice} fluid />
                     </div>
-                    <div className = "col-md-6 cols">
+                    <div className="col-md-6 cols">
                         <a href="#nothing"><h5>{this.props.p}</h5></a>
                         <h9>{this.props.info}</h9>
                     </div>
