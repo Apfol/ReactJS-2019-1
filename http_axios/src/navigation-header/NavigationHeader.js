42f85e3cf69abf852cd125e5345fdf094e6d40c6
Apfol <andresram@unisabana.edu.co>
2019-09-20 18:47:29 -0500

    Finished HTTP project with Axios (Basic CSS Design)

--- http_axios/src/navigation-header/NavigationHeader.js
+++ http_axios/src/navigation-header/NavigationHeader.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
+import logo from '../Users-icon.png';
 
 const NavigationHeader = () => {
     return (
         <div>
             <Navbar bg="light">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Brand href="#home">
+                    <img
+                        alt=""
+                        src={logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />
+                    {' Users List'}
+                </Navbar.Brand>
             </Navbar>
         </div>
     )
