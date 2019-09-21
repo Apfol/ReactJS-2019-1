4e3c4a03cd64f7854ef680cae02fcb3d3f226d49
danielNempeque <daniel.nempeque@gmail.com>
2019-09-21 16:48:40 -0500

    Removing duplicated instances

--- basic-routing/src/AppWithRouting/App.js
+++ basic-routing/src/AppWithRouting/App.js
@@ -1,63 +1,71 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link} from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Posts from './Posts/Posts';
 import FullPost from './FullPost/FullPost';
 import NewPost from './NewPost/NewPost';
+import axios from './axiosInstance';
 import './App.css';
 
 class App extends Component {
   state = {
-    posts: [
-      {
-        title: "First Title",
-        author: "Cristian",
-        content: "Lorem ipsum dolor sit amet, consectetur "+
-          "adipiscing elit, sed do eiusmod tempor incididunt "+
-          "ut labore et dolore magna aliqua. Ut enim ad minim "+
-          "veniam, quis nostrud exercitation ullamco laboris "+
-          "nisi ut aliquip ex ea commodo consequat."
-      },
-      {
-        title: "Second Title",
-        author: "Mike",
-        content: "Duis aute irure dolor in reprehenderit in "+
-        "voluptate velit esse cillum dolore eu fugiat nulla "+
-        "pariatur. Excepteur sint occaecat cupidatat non proident, "+
-        "sunt in culpa qui officia deserunt mollit anim id est laborum."
-      },
-      {
-        title: "Third Title",
-        author: "Juan",
-        content: "Sed ut perspiciatis unde omnis iste natus error "+
-        "sit voluptatem accusantium doloremque laudantium, totam rem "+
-        "aperiam, eaque ipsa quae ab illo inventore veritatis et quasi "+
-        "architecto beatae vitae dicta sunt explicabo."
-      },
-      {
-        title: "Forth Title",
-        author: "Ana",
-        content: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur "+
-        "aut odit aut fugit, sed quia consequuntur magni dolores eos qui "+
-        "ratione voluptatem sequi nesciunt."
-      }
-    ],
+    posts: [],
     newPostInfo: {
       title: "",
       author: "",
       content: ""
-    }
+    },
+    loading: false,
+    error: false
+  }
+
+  componentDidMount() {
+    this.setState({
+      loading: true
+    });
+    axios.get("/posts")
+      .then(response => {
+        var updatedPost = response.data.slice(0, 4);
+        updatedPost = updatedPost.map(post => {
+          return {
+            author: post.author,
+            title: post.title.slice(0, 6),
+            content: post.body
+          }
+        })
+        this.setState({
+          posts: updatedPost
+        });
+
+        console.log(updatedPost);
+      }).catch(error => {
+        this.setState({
+          error
+        });
+      }).then(() => {
+        this.setState({
+          loading: false
+        });
+      })
   }
 
   componentShouldUpdate(nextProps, nextState) {
     return nextState.openPostIndex !== this.state.openPostIndex;
   }
 
-  render () {
-    return(
+  render() {
+    var content;
+    if (this.state.loading) {
+      content = <h1>Loading...</h1>
+    } else if (this.state.error) {
+      content = <h1>Error</h1>
+    } else {
+      content = <Posts posts={this.state.posts} />
+    }
+    return (
       <BrowserRouter>
         <div>
           <header>
-            <nav className = "nav-bar">
+            <nav className="nav-bar">
               <ul>
                 {/* <li><a href = "/">Home</a></li> */}
                 <li><Link to="/">Home</Link></li>
@@ -66,18 +74,18 @@ class App extends Component {
               </ul>
             </nav>
           </header>
-          <h1 className = "main-header">My posts</h1>
+          <h1 className="main-header">My posts</h1>
 
-          <Route path = "/new-post" render = {() => (
-              <NewPost
-                  newPostInfo = {this.state.newPostInfo}
-                  updateNewPostData = {this.updateNewPostData}
-                  submitNewPost = {this.submitNewPost}
-              />
+          <Route path="/new-post" render={() => (
+            <NewPost
+              newPostInfo={this.state.newPostInfo}
+              updateNewPostData={this.updateNewPostData}
+              submitNewPost={this.submitNewPost}
+            />
           )} />
-          <Route path ="/" exact render = {() => <Posts posts = {this.state.posts} />} />
-          <Route path ="/post/:postIndex" exact render = {() => (
-              <FullPost openPost = {(postIndex) => this.openPost(postIndex)} />
+          <Route path="/" exact render={() => content} />
+          <Route path="/post/:postIndex" exact render={() => (
+            <FullPost openPost={(postIndex) => this.openPost(postIndex)} />
           )} />
         </div>
       </BrowserRouter>
@@ -102,7 +110,7 @@ class App extends Component {
 
   submitNewPost = () => {
     var updatedPosts = [...this.state.posts];
-    var newPostInfo = {...this.state.newPostInfo}
+    var newPostInfo = { ...this.state.newPostInfo }
 
     updatedPosts.push(newPostInfo);
 
@@ -114,6 +122,10 @@ class App extends Component {
         content: ""
       }
     })
+    axios.post("https://jsonplaceholder.typicode.com/posts", newPostInfo)
+      .then(response => {
+        console.log(response)
+      });
   }
 }
 
