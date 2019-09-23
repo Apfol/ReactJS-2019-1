b6589528ffc2843d160ae9eb61533ee5750bd057
danielNempeque <daniel.nempeque@gmail.com>
2019-09-22 20:22:05 -0500

    Adding error handling

--- http_workshop/src/components/Home/Home.js
+++ http_workshop/src/components/Home/Home.js
@@ -5,7 +5,8 @@ import axios from '../../AxiosInstance';
 class Home extends Component {
 	state = {
 		games: [],
-		loading: false
+		loading: false,
+		error: false
 	}
 	componentDidMount() {
 		this.setState({
@@ -17,16 +18,35 @@ class Home extends Component {
 					games: response.data
 				});
 			})
+			.catch(error => {
+				this.setState({
+					error: true
+				});
+			})
+			.then(() => {
+				this.setState({
+					loading: false
+				});
+			});
 	}
 	openPost = (postIndex) => {
 		return this.state.posts[postIndex];
 	}
 	render() {
+		var content;
+		if (this.state.loading) {
+			content = <h1>Loading...</h1>
+		} else if (this.state.error) {
+			content = <h1>Error loading data, please try again</h1>
+		} else {
+			content = <List games={this.state.games} />
+		}
+
 		return (
 			<div className={classes.home}>
 				<div className={classes["game-list"]}>
 					<h1 className={classes["pc-title"]}>Games</h1>
-					<List games={this.state.games} />
+					{content}
 				</div>
 			</div >
 		);
