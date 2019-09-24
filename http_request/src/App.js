467735b507d436531646245ddb1fcce53a0ba33d
Nathalia <indigodreamer11@hotmail.com>
2019-09-24 16:59:16 -0500

    Updated list

--- http_request/src/App.js
+++ http_request/src/App.js
@@ -6,21 +6,44 @@ class App extends Component {
   constructor () {
     super()
     this.state={
-      username:''
+      username:'',
+      email:'',
+      company:'', 
+      followers:'',
+      public_repos:'',
+      following:'',
+      avatar_url:'',
+      blog:'',
+      login:'',
+      created_at:''
+
+
     }
     this.handleClick = this.handleClick.bind(this)
   }
   handleClick () {
     axios.get('https://api.github.com/users/indigoshakti47')
-    .then(response => this.setState({username: response.data.name}))
+    .then(response => this.setState({username: response.data.name, email: response.data.bio, company: response.data.company, followers: response.data.followers, public_repos: response.data.public_repos, following: response.data.following, avatar_url:response.data.avatar_url, blog:response.data.blog, login: response.data.login, created_at: response.data.created_at
+    }))
   }
+
   render () {
     return (
       <div className='button__container'>
       <button className='button' onClick={this.handleClick}>
-        Click Me
+        Click para revelar usuario del mes
       </button>
-      <p>{this.state.username}</p>
+      <p>Nombre = {this.state.username}</p>
+      <p>Email = {this.state.email}</p>
+      <p>Empresa = {this.state.company}</p>
+      <p>Seguidores = {this.state.followers}</p>
+      <p>Numero de repositorios = {this.state.public_repos}</p>
+      <p>Siguiendo = {this.state.following}</p>
+      <p> Avatar = {this.state.avatar_url}</p>
+      <p> LinkedIn = {this.state.blog}</p>
+      <p>Usuario = {this.state.login}</p>
+      <p> Fecha de creación = {this.state.created_at}</p>
+
     </div>
     )
   }
