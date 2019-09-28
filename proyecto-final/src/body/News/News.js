e7466ff4effd1af78401a90906feb567d31b324a
Apfol <andresram@unisabana.edu.co>
2019-09-28 13:06:24 -0500

    Added CSS Modules in News.js

--- proyecto-final/src/body/News/News.js
+++ proyecto-final/src/body/News/News.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
-import "./News.css"
+import classes from "./News.css";
 
 class News extends React.Component {
     render = () => {
         return (
-            <li class="nav-item noticia">
-                <div className="row notices">
-                    <div className="col-md-6 cols">
-                        <Image className="noticeImg" src={this.props.notice} fluid />
+            <li class={`nav-item ${classes.noticia}`}>
+                <div className={`row ${classes.notices}`}>
+                    <div className={`col-md-6 ${classes.cols}`}>
+                        <Image className={classes.noticeImg} src={this.props.notice} fluid />
                     </div>
-                    <div className="col-md-6 cols">
+                    <div className={`col-md-6 ${classes.cols}`}>
                         <a href="#nothing"><h5>{this.props.p}</h5></a>
                         <h9>{this.props.info}</h9>
                     </div>
