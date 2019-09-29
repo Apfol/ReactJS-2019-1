5203d9f2788a31a0cead855078eb5734415820ab
eduardriar <eduardriar@unisabana.edu.co>
2019-09-29 16:45:58 -0500

    .

--- proyecto_bienestar/src/App.js
+++ proyecto_bienestar/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
 import MissingObjectList from './Resources/Components/MissingObjectList';
-import SportsEquipment from './Resources/Components/SportsEquipment';
+//import SportsEquipment from './Resources/Components/SportsEquipment';
 import SideBar from './Resources/Components/SideBar';
-import NavBarComponent from './Resources/Components/NavBar';
+//import NavBarComponent from './Resources/Components/NavBar';
 
 
 function App() {
-   const objects=[{ SportsElement: "Balon de Futbol",
+   /*const objects=[{ SportsElement: "Balon de Futbol",
   SportsElement1: "Balon de Basket", 
-  SportsElement2: "Remos" }]
+  SportsElement2: "Remos" }]*/
 
   const object = [
     {name: "Calculadora", 
