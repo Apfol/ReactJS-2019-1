840189d6279dccebcff49a4c5c1828a6dbfbcc4a
eduardriar <eduardriar@unisabana.edu.co>
2019-10-01 10:58:21 -0500

    Ya todo

---
+++ proyecto_bienestar/src/Resources/Components/Data/AxiosInstance.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+const instance = axios.create({
+    baseURL: "http://my-json-server.typicode.com/DanielR9808/jsonServer/"
+});
+export default instance;
--- proyecto_bienestar/src/Resources/Components/FoundedObjectsList.js
+++ proyecto_bienestar/src/Resources/Components/FoundedObjectsList.js
@@ -2,21 +2,54 @@ import React, { Component } from "react";
 import classes from "./FoundedObjectsList.css";
 import FoundedObject from "./FoundedObject";
 import SearchFilter from "./SearchFilter";
+import axios from "./Data/AxiosInstance"
 
 
-class FoundedObjectsList extends Component {
-    state = {
-        objects:[
-          {name: "Celular Xyz", location: "Biblioteca", by: "Cristian"},                             
-        ]
+class FoundedObjectsList extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      objects : {
+        name: "",
+        location: "",
+        foundedby: "",
+        image: "" ,
+        estado:""
       }
+    };
+  }
+    
+  componentDidMount(){
+
+    axios.get('/missingObject')
+    .then(response => {      
+      console.log(response);
+      const post = response.data.map(posts =>{
+        return {
+          name: posts.name,
+          location: posts.location  ,
+          foundedby:posts.foundedby ,
+          image:posts.image,
+          estado: posts.estado
+        }        
+      });
+      this.setState(this.objects.MissingObjectList = post);
+      console.log(post);
+  });
+}
+
+objects = {
+  MissingObjectList:[]
+}
+
   render() {
     return (      
       <div className={classes.MissingObjectList}>          
           <SearchFilter/>
           <h1 className={classes.Title}>Has encontrado estos objetos</h1>
-            {this.state.objects.map(object => <FoundedObject name = {object.name} location = {object.location} 
-         by = {object.by}/>)} 
+            {this.objects.MissingObjectList.map(object => <FoundedObject name = {object.name} location = {object.location} 
+         foundedby = {object.foundedby} image = {object.image}/>)} 
       </div>
     );
   }
