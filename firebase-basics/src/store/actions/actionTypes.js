1c32918c1fd074681f0ab0b360456eaff2897ef8
Cristian Paredes <cristian.paredes@wnco.com>
2019-10-28 10:58:01 -0500

    Accessing the web - Fetching and saving posts to firebase.

--- firebase-basics/src/store/actions/actionTypes.js
+++ firebase-basics/src/store/actions/actionTypes.js
@@ -1,4 +1,6 @@
 export const LOGIN = 'LOGIN';
 export const SIGN_IN = 'SIGN_IN';
 export const LOG_OUT = 'LOG_OUT';
+
 export const SAVE_POST = 'SAVE_POST';
+export const FETCH_POSTS = 'FETCH_POSTS';
