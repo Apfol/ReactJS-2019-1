d09fe2af8e5bdaf7918636ccef80c1a37ded88ab
Nathalia <indigodreamer11@hotmail.com>
2019-11-03 11:39:46 -0500

    Firebase - Taller

---
+++ firebase-basics/src/store/actions/actionTypes.js
@@ -0,0 +1,10 @@
+export const LOGIN = 'LOGIN';
+export const SIGN_IN = 'SIGN_IN';
+export const LOG_OUT = 'LOG_OUT';
+export const LOGIN_ERROR = 'LOGIN_ERROR';
+
+export const SAVE_POST = 'SAVE_POST';
+export const FETCH_POSTS = 'FETCH_POSTS';
+
+export const START_LOADING = 'START_LOADING';
+export const END_LOADING = 'END_LOADING';
---
+++ firebase-basics/src/store/reducers/authentication.js
@@ -0,0 +1,72 @@
+import * as actionTypes from '../actions/actionTypes';
+import updateObject from '../utility';
+
+const initialState = {
+    isUserLoggedIn: false,
+    userLoggedIn: {
+        userName: '',
+        idToken: '',
+        localId: ''
+    },
+    loadingAuth: false,
+    logErro: false
+}
+
+const login = (state, action) => {
+    return updateObject(state, {
+        isUserLoggedIn: true,
+        userLoggedIn: {
+            userName: action.payload.userName,
+            idToken: action.payload.idToken,
+            localId: action.payload.localId
+        }
+    });
+}
+
+const signIn = (state, action) => {
+    return updateObject(state, {
+        isUserLoggedIn: true,
+        userLoggedIn: {
+            userName: action.payload.userName,
+            idToken: action.payload.idToken,
+            localId: action.payload.localId
+        }
+    });
+}
+
+const logOut = (state, action) => {
+    return updateObject(state, {
+        isUserLoggedIn: false,
+        userLoggedIn: {
+            userName: '',
+            idToken: '',
+            localId: ''
+        }
+    });
+}
+
+const startLoading = (state, action) => {
+    return updateObject(state, { loadingAuth: true });
+}
+
+const endLoading = (state, action) => {
+    return updateObject(state, { loadingAuth: false });
+}
+
+const logError = (state, action) => {
+    return updateObject(state, { logErro: !state.logErro });
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.LOGIN: return login(state, action);
+        case actionTypes.SIGN_IN: return signIn(state, action);
+        case actionTypes.LOG_OUT: return logOut(state, action);
+        case actionTypes.START_LOADING: return startLoading(state, action);
+        case actionTypes.END_LOADING: return endLoading(state, action);
+        case actionTypes.LOGIN_ERROR: return logError(state, action);
+        default: return state;
+    }
+}
+
+export default reducer;
