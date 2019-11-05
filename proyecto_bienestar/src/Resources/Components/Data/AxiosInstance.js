fbd53b26489905b313d80e52688ea0a84a61287c
DanielR9808 <danielroba@unisabana.edu.co>
2019-11-05 13:14:45 -0500

    Prueba

--- proyecto_bienestar/src/Resources/Components/Data/AxiosInstance.js
+++ proyecto_bienestar/src/Resources/Components/Data/AxiosInstance.js
@@ -2,4 +2,4 @@ import axios from 'axios';
 const instance = axios.create({
     baseURL: "http://my-json-server.typicode.com/DanielR9808/jsonServer/"
 });
-export default instance;
+export default instance; 
