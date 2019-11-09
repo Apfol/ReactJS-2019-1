3004149087c3c7d1989fea8decb71243bd3246a0
THEYAN <camilosk01@gmail.com>
2019-11-09 18:16:15 -0500

    cursos miniatura

--- curvery-firebase/src/components/VideoCards/CardPreview/CardSection.js
+++ curvery-firebase/src/components/VideoCards/CardPreview/CardSection.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import CardPreview from './CardPreview';
-import cardSection from './CardSection.css';
+import './CardSection.css';
 class CardSection extends Component{
     render(){
         return(
-            <div className={cardSection.body}>
-            <h1 className={cardSection.title}>Creativos que pueden interesarte</h1>
+            <div className="body_CardSection">
+            <h1 className="title_CardSection">Cursos que pueden interesarte</h1>
             <CardPreview></CardPreview>
             </div>
         )
