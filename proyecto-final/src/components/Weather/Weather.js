0ca9e60e935048491f2164274bdbc6bea3b2dc3c
AlejoCuPe <alejocp236@hotmail.com>
2019-11-12 15:39:16 -0500

    Minor Changes

--- proyecto-final/src/components/Weather/Weather.js
+++ proyecto-final/src/components/Weather/Weather.js
@@ -8,7 +8,7 @@ class Weather extends React.Component {
     }
 
     componentDidMount() {
-        const weatherURL = `http://api.openweathermap.org/data/2.5/weather?q=Bogota&appid=c2a3500b611bd3b748acd7255344ec4d&units=metric`;
+        const weatherURL = `http://api.openweathermap.org/data/2.5/weather?q=Sopó&appid=c2a3500b611bd3b748acd7255344ec4d&units=metric`;
 
         fetch(weatherURL)
             .then(res => res.json())
